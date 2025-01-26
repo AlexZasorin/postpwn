ba4909d7fcb6d048161edca572e72a0c9e5294f6
Alex Zasorin <zasorin.alek@gmail.com>
2025-01-25 20:41:24 -0800

    fix: filter for recurring tasks, exclude today, keep just overdue

--- src/that_what_must_be_done/__init__.py
+++ src/that_what_must_be_done/__init__.py
@@ -36,7 +36,7 @@ def main() -> None:
     asyncio.run(
         reschedule(
             api=api,
-            filter="!assigned to:others & !no date & (today | overdue) & recurring & no deadline & !p1",
+            filter="!assigned to:others & !no date & overdue & recurring & no deadline & !p1",
             max_weight=max_weight,
         )
     )
