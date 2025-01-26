106b545c6594df6ef5978c2badbc0e8be1138d96
Alex Zasorin <zasorin.alek@gmail.com>
2025-01-25 21:26:48 -0800

    feat: update deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-# Make sure we start in the correct directory
-cd "$(dirname "$0")/.."
 
-docker compose pull
+{
+  echo "=== Deployment sync started ==="
+
+  # Make sure we start in the correct directory
+  cd "$(dirname "$0")/.."
+
+  docker compose pull
+
+  echo "=== Deployment sync finished ==="
+} 2>&1 | sed "s/^/$(date '+%Y-%m-%d %H:%M:%S') /"
