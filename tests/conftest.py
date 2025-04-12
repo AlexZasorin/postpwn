a6c0f4eb286ac7b19784c5096154760a39bac0cc
Alex Zasorin <zasorin.alek@gmail.com>
2025-04-12 08:34:15 +0000

    refactor: move task builders out pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,8 @@
 import asyncio
 from asyncio import AbstractEventLoop
-from typing import Any, Generator, Optional
+from typing import Generator
 
 import pytest
-from helpers.data_generators import (
-    generate_datetime,
-    generate_id,
-    generate_int,
-    generate_text,
-    generate_timezone,
-    generate_url,
-)
-from todoist_api_python.models import Due, Duration, Task
 
 
 @pytest.fixture
@@ -19,78 +10,3 @@ def event_loop() -> Generator[AbstractEventLoop, None, None]:
     loop = asyncio.new_event_loop()
     yield loop
     loop.close()
-
-
-@pytest.fixture
-def due(properties: Optional[dict[str, Any]] = None) -> Due:
-    defaults = Due(
-        date=generate_datetime(),
-        is_recurring=False,
-        datetime=generate_datetime(before_now=False, after_now=True),
-        string=generate_text(),
-        timezone=generate_timezone(),
-    )
-
-    if properties is None:
-        return defaults
-
-    return Due(
-        **{
-            field: getattr(properties, field) or getattr(defaults, field)
-            for field in defaults.__dict__
-        }
-    )
-
-
-@pytest.fixture
-def duration(properties: Optional[dict[str, Any]] = None) -> Duration:
-    defaults = Duration(
-        amount=generate_int(),
-        # TODO: Create a separate fixture for generating random string literals
-        # instead of using text fixture
-        unit=generate_text(words=1, ext_word_list=["minute", "day"]),
-    )
-
-    if properties is None:
-        return defaults
-
-    return Duration(
-        **{
-            field: getattr(properties, field) or getattr(defaults, field)
-            for field in defaults.__dict__
-        }
-    )
-
-
-@pytest.fixture
-def task(properties: Optional[dict[str, Any]] = None) -> Task:
-    defaults = Task(
-        assignee_id=generate_id(),
-        assigner_id=generate_id(),
-        comment_count=generate_int(),
-        is_completed=False,
-        content=generate_text(),
-        created_at=generate_datetime(),
-        creator_id=generate_id(),
-        description=generate_text(),
-        due=due(),
-        id=generate_id(),
-        labels=[generate_text(words=1) for _ in range(3)],
-        order=generate_int(),
-        parent_id=generate_id(),
-        priority=1,
-        project_id=generate_id(),
-        section_id=generate_id(),
-        url=generate_url(),
-        duration=duration(),
-    )
-
-    if properties is None:
-        return defaults
-
-    return Task(
-        **{
-            field: getattr(properties, field) or getattr(defaults, field)
-            for field in defaults.__dict__
-        }
-    )
--- tests/helpers/data_generators.py
+++ tests/helpers/data_generators.py
@@ -1,6 +1,8 @@
 import random
+from typing import Any, Optional
 
 from faker import Faker
+from todoist_api_python.models import Due, Duration, Task
 
 
 def generate_id() -> str:
@@ -22,12 +24,18 @@ def generate_text(words: int = 10, ext_word_list: list[str] | None = None) -> st
 
 def generate_datetime(before_now: bool = True, after_now: bool = False) -> str:
     fake = Faker()
-    return str(fake.date_time_this_month(before_now=before_now, after_now=after_now))
+    return (
+        fake.date_time_this_month(before_now=before_now, after_now=after_now)
+        .replace(hour=12, minute=0, second=0, microsecond=0)
+        .strftime("%Y-%m-%dT%H:%M:%S")
+    )
 
 
-def generate_date() -> str:
+def generate_date(before_today: bool = True, after_today: bool = False) -> str:
     fake = Faker()
-    return str(fake.date_this_month(before_today=False, after_today=True))
+    return fake.date_this_month(
+        before_today=before_today, after_today=after_today
+    ).strftime("%Y-%m-%d")
 
 
 def generate_timezone() -> str:
@@ -38,3 +46,75 @@ def generate_timezone() -> str:
 def generate_url() -> str:
     fake = Faker()
     return fake.url()
+
+
+def build_due(properties: Optional[dict[str, Any]] = None) -> Due:
+    defaults = Due(
+        date=generate_date(),
+        is_recurring=False,
+        datetime=generate_datetime(),
+        string=generate_text(),
+        timezone=generate_timezone(),
+    )
+
+    if properties is None:
+        return defaults
+
+    return Due(
+        **{
+            field: getattr(properties, field) or getattr(defaults, field)
+            for field in defaults.__dict__
+        }
+    )
+
+
+def build_duration(properties: Optional[dict[str, Any]] = None) -> Duration:
+    defaults = Duration(
+        amount=generate_int(),
+        # TODO: Create a separate fixture for generating random string literals
+        # instead of using text fixture
+        unit=generate_text(words=1, ext_word_list=["minute", "day"]),
+    )
+
+    if properties is None:
+        return defaults
+
+    return Duration(
+        **{
+            field: getattr(properties, field) or getattr(defaults, field)
+            for field in defaults.__dict__
+        }
+    )
+
+
+def build_task(properties: Optional[dict[str, Any]] = None) -> Task:
+    defaults = Task(
+        assignee_id=generate_id(),
+        assigner_id=generate_id(),
+        comment_count=generate_int(),
+        is_completed=False,
+        content=generate_text(),
+        created_at=generate_datetime(),
+        creator_id=generate_id(),
+        description=generate_text(),
+        due=build_due(),
+        id=generate_id(),
+        labels=[generate_text(words=1) for _ in range(3)],
+        order=generate_int(),
+        parent_id=generate_id(),
+        priority=1,
+        project_id=generate_id(),
+        section_id=generate_id(),
+        url=generate_url(),
+        duration=build_duration(),
+    )
+
+    if properties is None:
+        return defaults
+
+    return Task(
+        **{
+            field: getattr(properties, field) or getattr(defaults, field)
+            for field in defaults.__dict__
+        }
+    )
