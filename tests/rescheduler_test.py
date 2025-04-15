0b1982cb1cd19619f9d89e6f2fdaac49029cb65d
Alex Zasorin <zasorin.alek@gmail.com>
2025-04-15 15:19:50 -0700

    test: add basic rescheduling with rules test

--- tests/rescheduler_test.py
+++ tests/rescheduler_test.py
@@ -1,10 +1,12 @@
 import asyncio
+from collections import defaultdict
 import logging
 import sys
 from asyncio import AbstractEventLoop
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
+from todoist_api_python.models import Task
 from helpers.data_generators import build_task
 from helpers.set_env import set_env
 from requests import HTTPError
@@ -118,34 +120,65 @@ def test_reschedule_with_rules(event_loop: AbstractEventLoop) -> None:
     fake_api = FakeTodoistAPI("VALID_TOKEN")
 
     tasks = [
-        build_task({"labels": ["weight_one"]}),
-        build_task({"labels": ["weight_one"]}),
-        build_task({"labels": ["weight_one"]}),
-        build_task({"labels": ["weight_two"]}),
+        *[build_task({"labels": ["weight_one"]}) for _ in range(2)],
+        *[build_task({"labels": ["weight_two"]}) for _ in range(2)],
     ]
 
     fake_api.setup_tasks(tasks)
 
-    curr_date = datetime(2025, 1, 5).date()
+    curr_datetime = datetime(2025, 1, 5, 12, 0, 0)
 
     with set_env({"RETRY_ATTEMPTS": "1"}):
-        postpwn(fake_api, event_loop, curr_date, **kwargs)
+        postpwn(fake_api, event_loop, curr_datetime.date(), **kwargs)
 
-    call_args_dict = {}
-    for call_arg in fake_api.update_task.call_args_list:
-        logger.info(call_arg[0])
-        # call_arg[call_arg.kwargs[""]]
-
-    # logger.info(fake_api.update_task.call_args_list)
     assert fake_api.update_task.call_count == 4
-    # assert (
-    #     fake_api.update_task.call_args_list[0].kwargs["due_datetime"]
-    #     == "2025-01-02T12:00:00"
-    # )
-    # assert (
-    #     fake_api.update_task.call_args_list[1].kwargs["due_datetime"]
-    #     == "2025-01-03T12:00:00"
-    # )
+
+    # Group tasks by due_datetime
+    calls = fake_api.update_task.call_args_list
+    scheduled_dates: dict[str, list[str]] = defaultdict(list)
+
+    for call in calls:
+        task_id = call.args[0]
+        due_datetime = call.kwargs["due_datetime"]
+
+        matching_task = next(t for t in tasks if t.id == task_id)
+        task_label = (
+            "weight_one"
+            if matching_task.labels and "weight_one" in matching_task.labels
+            else "weight_two"
+        )
+        task_label = (
+            next(label for label in matching_task.labels)
+            if matching_task.labels
+            else None
+        )
+
+        if task_label:
+            scheduled_dates[due_datetime].append(task_label)
+
+    format = "%Y-%m-%dT%H:%M:%S"
+
+    # Current day (Jan 5)
+    curr_date_str = curr_datetime.strftime(format)
+    assert curr_date_str in scheduled_dates
+    assert scheduled_dates[curr_date_str].count("weight_one") == 2
+    assert scheduled_dates[curr_date_str].count("weight_two") == 0
+
+    # Next day (Jan 6)
+    next_day = (curr_datetime + timedelta(days=1)).strftime(format)
+    assert next_day in scheduled_dates
+    assert (
+        scheduled_dates[next_day].count("weight_one") == 0
+        and scheduled_dates[next_day].count("weight_two") == 1
+    )
+
+    # Day after next (Jan 7)
+    third_day = (curr_datetime + timedelta(days=2)).strftime(format)
+    assert third_day in scheduled_dates
+    assert (
+        scheduled_dates[third_day].count("weight_one") == 0
+        and scheduled_dates[third_day].count("weight_two") == 1
+    )
 
 
 # TODO: Tasks with labels that do not match the rules should not be rescheduled
