b0ada20a2b82aa95e4db178fe6c1fe87a97f2b37
Alex Zasorin <zasorin.alek@gmail.com>
2025-04-17 00:06:35 -0700

    fix: broken cron string regex

--- src/postpwn/validation.py
+++ src/postpwn/validation.py
@@ -1,1 +1,1 @@
-CRON_SCHEDULE_REGEX = r"/(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)|((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5,7})/"
+CRON_SCHEDULE_REGEX = r"(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)|((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5,7})"
