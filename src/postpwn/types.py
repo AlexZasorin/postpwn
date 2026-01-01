0b607d4c324be9a73488620acd48090ba929b335
Alex Zasorin <zasorin.alek@gmail.com>
2026-01-01 13:54:37 -0800

    chore: remove unused UpdateTaskParams TypedDict

--- src/postpwn/types.py
+++ src/postpwn/types.py
@@ -21,17 +21,3 @@ class WeightConfig(BaseModel):
 class ScheduleConfig(BaseModel):
     max_weight: WeightConfig | int
     rules: list[Rule]
-
-
-class UpdateTaskParams(TypedDict, total=False):
-    content: str
-    description: str
-    labels: list[str]
-    priority: int
-    due_string: str
-    due_date: str
-    due_datetime: str
-    due_lang: str
-    assignee_id: str
-    duration: int
-    duration_unit: str
